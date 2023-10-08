4301081e7bf40d4f2a0d38e435e6238b980ba6e4
Austin Meadows <slowpoke@slowp.ke>
2023-10-08 13:04:53 -0400

    🚀 init repo

---
+++ astro.config.ts
@@ -0,0 +1,10 @@
+import astro_sitemap from "@astrojs/sitemap";
+import astro_compress from "astro-compress";
+import { defineConfig } from "astro/config";
+import astro_uno from "unocss/astro";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://slowp.ke",
+	integrations: [astro_sitemap(), astro_uno(), astro_compress({ Logger: 2 })]
+});
---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+	plugins: [
+		require("postcss-variable-compress"),
+		require("postcss-lightningcss")({
+			browsers: ">= 0.75%"
+		}),
+		require("postcss-drop-empty-css-vars")
+	]
+};

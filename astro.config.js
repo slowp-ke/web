3b15d349f7cd984cd27f44ac3ad12f324cd16367
Austin Raey <slowpoke@slowp.ke>
2024-12-04 13:29:30 -0500

    🔧 adjust build settings

--- astro.config.js
+++ astro.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	build: {
+		inlineStylesheets: "always"
+	},
+
 	integrations: [tailwind()],
 
 	output: "static",

32d0523fcfcfe4e2cf2aa8ad2d2e0c67e30bbde5
Austin Raey <slowpoke@slowp.ke>
2026-02-23 11:52:21 -0500

    📦 package update, remove problematic packages

--- astro.config.js
+++ astro.config.js
@@ -1,4 +1,3 @@
-import compress from "@playform/compress";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -12,21 +11,6 @@ export default defineConfig({
 			directives: ["default-src 'none'", "img-src data: 'self'"]
 		}
 	},
-
-	integrations: [
-		compress({
-			HTML: {
-				"html-minifier-terser": {
-					// The following attributes must be set to `false` or Astro's injected CSP will break.
-					collapseWhitespace: false,
-					minifyCSS: false,
-					minifyJS: false,
-					minifyURLs: true,
-					removeComments: true
-				}
-			}
-		})
-	],
 
 	output: "static",
 
